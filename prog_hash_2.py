5f42765475d15e35cf9ec36d3058cbdcb12acee9
Yuyeon <63159699+yuyeonee@users.noreply.github.com>
2023-10-19 19:57:08 +0900

    전화번호 목록
    string sort하면 그냥 사전순으로 정리됨..

---
+++ prog_hash_2.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def solution(phone_book):
+    answer = True
+
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    
+    return answer
